2f789eb87fc7dd96faade58251a7d76e7522fb18
Ana Pacheco <kornetera@gmail.com>
2023-03-02 20:57:14 +0000

    feat traffic light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,15 +1,42 @@
-import React from "react";
-
-
-
+import React, {useState, useEffect} from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			
-		</div>
-	);
+
+	const [color, setColor] = useState({
+    rojo:"",
+    ambar: "",
+    verde:""
+	});
+
+  useEffect(()=>{
+    setColor({rojo:"brillo"}) 
+  },[])
+
+
+  return <div className="container">
+      <div class={"rojo " + color.rojo } onClick ={() => setColor({
+        rojo:"brillo",
+        ambar: "",
+        verde:""
+
+      })} > 
+      </div>
+      <div  class={"ambar " + color.ambar} onClick ={() => setColor({
+        rojo:"",
+        ambar: "brillo",
+        verde:""
+      })}  >
+      </div>
+	  <div class={"verde " + color.verde}onClick ={() => setColor({
+        rojo:"",
+        ambar: "",
+        verde:"brillo"
+      })} >
+      </div>
+    </div>
+
+  
 };
 
 export default Home;
