45dd49e6442ed9dfa8666d173608edccc4ccac05
Ana Pacheco <kornetera@gmail.com>
2023-03-03 12:50:55 +0000

    minor changes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,41 +2,60 @@ import React, {useState, useEffect} from "react";
 
 //create your first component
 const Home = () => {
-
-	const [color, setColor] = useState({
-    rojo:"",
+  const [color, setColor] = useState({
+    purpura: "",
+    rojo: "",
     ambar: "",
-    verde:""
-	});
+    verde: "",
+  });
 
-  useEffect(()=>{
-    setColor({rojo:"brillo"}) 
-  },[])
+  useEffect(() => {
+    setColor({rojo: "brillo"});
+  }, []);
+
+  return (
+  <>  <div className="container">
 
 
-  return <div className="container">
-      <div class={"rojo " + color.rojo } onClick ={() => setColor({
-        rojo:"brillo",
+  <div
+    className={"rojo " + color.rojo}
+    onClick={() =>
+      setColor({
+        purpura: "",
+        rojo: "brillo-rojo",
         ambar: "",
-        verde:""
-
-      })} > 
-      </div>
-      <div  class={"ambar " + color.ambar} onClick ={() => setColor({
-        rojo:"",
-        ambar: "brillo",
-        verde:""
-      })}  >
-      </div>
-	  <div class={"verde " + color.verde}onClick ={() => setColor({
-        rojo:"",
+        verde: "",
+      })
+    }
+  ></div>
+  <div
+    className={"ambar " + color.ambar}
+    onClick={() =>
+      setColor({
+        purpura: "",
+        rojo: "",
+        ambar: "brillo-ambar",
+        verde: "",
+      })
+    }
+  ></div>
+  <div
+    className={"verde " + color.verde}
+    onClick={() =>
+      setColor({
+        purpura: "",
+        rojo: "",
         ambar: "",
-        verde:"brillo"
-      })} >
-      </div>
-    </div>
+        verde: "brillo-verde",
+      })
+    }
+  ></div>
+</div>
 
-  
+</>
+      
+
+  );
 };
 
 export default Home;
